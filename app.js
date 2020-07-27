84a95994526e30f029508094161116810ea56a77
Arefi6 <arefinhossain3@gmail.com>
2020-07-27 12:15:38 +0600

    number expection

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ const plusBtn = document.getElementById(id).addEventListener("click",function(){
     const totalPrice= (qtyNumber+1)*priceDeafault;
     console.log(totalPrice);
      const setPrice = document.getElementById(priceId).innerText = totalPrice;
+      return totalPrice;
+     //document.getElementById("sub-total").innerText=totalPrice;
      
-     //console.log(test);    
+     //console.log(test);         
+    // subTotal(item1,item2);
+     
 });
 }
 //minus Event 
@@ -32,12 +36,42 @@ function decrementQty (id,qtyId,priceId,priceDeafault) {
          //console.log(test);    
   });
 }
+
+
 //calling function 
-incrementQty("plus-btn","qty","price",1219);
-incrementQty("plus-btn-2","qty-2","price-2",59);
+  incrementQty("plus-btn","qty","price",1219);
+  incrementQty("plus-btn-2","qty-2","price-2",59);
+
+ const item1 = document.getElementById("price").value;
+ let parseNumber1 = parseInt(item1);  
+ const item2 = document.getElementById("price-2").value;
+ let parseNumber2=parseInt(item2);
+ const subTotal = parseNumber1 + parseNumber2;
+ console.log(subTotal);
+ document.getElementById('sub-total').innerText = subTotal;
+ 
+
+ //subTotal(arr);
+//  function subTotal(arr){
+    
+//    
+// }
 //Decrements
 decrementQty("minus-btn","qty","price",1219);
  decrementQty("minus-btn-2","qty-2","price-2",59);
 
+// function totalCalculation(item1,item2){
+//   const subTotal = document.getElementById("sub-total").innerText;
+//   const  Price = document.getElementById("price").innerText;
+//   const priceNumber = parseInt(Price);
+//   console.log(priceNumber);
 
+//   const  Price2 = document.getElementById("price-2").innerText;
+//   const priceNumber2 = parseInt(Price);
+
+//   console.log();
+//   //const subTotalNumber = parseInt(subTotal); 
+  
+//   //console.log(subTotal); 
+// }
 
